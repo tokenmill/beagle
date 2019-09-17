ae3e1804ce36863551dbeaffceb40ca1af7216c8
Dainius Jocas <dainius@tokenmill.com>
2019-09-17 12:27:15 +0300

    feat: dictionary features

--- src/beagle/java.clj
+++ src/beagle/java.clj
@@ -1,5 +1,4 @@
 (ns beagle.java
-  (:gen-class)
   (:require [beagle.phrases :as phrases]))
 
 (gen-class
@@ -7,7 +6,25 @@
   :state "state"
   :init "init"
   :constructors {[String] []}
-  :methods [[text [] String]]
+  :methods [[text [] String]
+            [type [] String]
+            [setType [String] void]
+            [id [] String]
+            [setId [String] void]
+            [synonyms [] java.util.Collection]
+            [setSynonyms [java.util.Collection] void]
+            [caseSensitive [] Boolean]
+            [setCaseSensitive [Boolean] void]
+            [asciiFold [] Boolean]
+            [setAsciiFold [Boolean] void]
+            [stem [] Boolean]
+            [setStem [Boolean] void]
+            [stemmer [] String]
+            [setStemmer [String] void]
+            [slop [] Integer]
+            [setSlop [Integer] void]
+            [meta [] java.util.Map]
+            [setMeta [java.util.Map] void]]
   :prefix DictionaryEntry-)
 
 (defn DictionaryEntry-init [phrase]
@@ -15,6 +32,42 @@
 
 (defn DictionaryEntry-text [this]
   (@(.state this) :text))
+(defn DictionaryEntry-type [this]
+  (@(.state this) :type))
+(defn DictionaryEntry-setType [this type]
+  (swap! (.state this) assoc :type type))
+(defn DictionaryEntry-id [this]
+  (@(.state this) :id))
+(defn DictionaryEntry-setId [this id]
+  (swap! (.state this) assoc :id id))
+(defn DictionaryEntry-synonyms [this]
+  (@(.state this) :synonyms))
+(defn DictionaryEntry-setSynonyms [this synonyms]
+  (swap! (.state this) assoc :synonyms synonyms))
+(defn DictionaryEntry-caseSensitive [this]
+  (@(.state this) :case-sensitive?))
+(defn DictionaryEntry-setCaseSensitive [this case-sensitive]
+  (swap! (.state this) assoc :case-sensitive? case-sensitive))
+(defn DictionaryEntry-asciiFold [this]
+  (@(.state this) :ascii-fold?))
+(defn DictionaryEntry-setAsciiFold [this ascii-fold]
+  (swap! (.state this) assoc :ascii-fold? ascii-fold))
+(defn DictionaryEntry-stem [this]
+  (@(.state this) :stem?))
+(defn DictionaryEntry-setStem [this stem]
+  (swap! (.state this) assoc :stem? stem))
+(defn DictionaryEntry-stemmer [this]
+  (@(.state this) :stemmer))
+(defn DictionaryEntry-setStemmer [this stemmer]
+  (swap! (.state this) assoc :stemmer stemmer))
+(defn DictionaryEntry-slop [this]
+  (@(.state this) :slop))
+(defn DictionaryEntry-setSlop [this slop]
+  (swap! (.state this) assoc :slop slop))
+(defn DictionaryEntry-meta [this]
+  (@(.state this) :meta))
+(defn DictionaryEntry-setMeta [this meta]
+  (swap! (.state this) assoc :meta meta))
 
 
 (gen-class
