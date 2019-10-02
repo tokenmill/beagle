af8464ef5d5ac1c4962e6db17a91c988a397a3a3
Dainius Jocas <dainius.jocas@tokenmill.lt>
2019-10-02 13:07:21 +0300

    refactor: rename annotator to highlighter-fn

--- test/beagle/phrases_test.clj
+++ test/beagle/phrases_test.clj
@@ -22,8 +22,8 @@
 
 (deftest type-per-dictionary-entry
   (let [dictionary [{:text "test phrase" :id "1" :meta {:test "test"} :type "CUSTOM"}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated test phrase after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated test phrase after annotated")]
     (is (seq (s/conform ::schema/annotations anns)))
     (is (seq anns))
     (is (= "1" (-> anns first :dict-entry-id)))
@@ -33,160 +33,160 @@
 
 (deftest id
   (let [dictionary [{:text "test" :id "1" :meta {:test "test"}}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated test after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated test after annotated")]
     (is (seq anns))
     (is (= "1" (-> anns first :dict-entry-id)))
     (is (= "LABEL" (-> anns first :type)))))
 
 (deftest metadata-append
   (let [dictionary [{:text "test" :meta {"email" "test@example.com"}}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated test after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated test after annotated")]
     (is (seq anns))
     (is (= {"email" "test@example.com"} (-> anns first :meta)))))
 
 (deftest case-sensitivity
   (testing "case sensitive"
     (let [dictionary [{:text "test"}]
-          annotator (phrases/highlighter dictionary {:type-name label})
-          anns (annotator "before annotated test after annotated")]
+          highlighter-fn (phrases/highlighter dictionary {:type-name label})
+          anns (highlighter-fn "before annotated test after annotated")]
       (is (seq anns)))
     (let [dictionary [{:text "TeSt" :case-sensitive? true}]
-          annotator (phrases/highlighter dictionary {:type-name label})
-          anns (annotator "before annotated test after annotated")]
+          highlighter-fn (phrases/highlighter dictionary {:type-name label})
+          anns (highlighter-fn "before annotated test after annotated")]
       (is (empty? anns)))
     (let [label "LABEL"
           dictionary [{:text "test" :case-sensitive? true}]
-          annotator (phrases/highlighter dictionary {:type-name label})
-          anns (annotator "before annotated Test after annotated")]
+          highlighter-fn (phrases/highlighter dictionary {:type-name label})
+          anns (highlighter-fn "before annotated Test after annotated")]
       (is (empty? anns))))
 
   (testing "case insensitive"
     (let [dictionary [{:text "TeSt" :case-sensitive? false}]
-          annotator (phrases/highlighter dictionary {:type-name label})
-          anns (annotator "before annotated test after annotated")]
+          highlighter-fn (phrases/highlighter dictionary {:type-name label})
+          anns (highlighter-fn "before annotated test after annotated")]
       (is (seq anns)))
     (let [dictionary [{:text "test" :case-sensitive? false}]
-          annotator (phrases/highlighter dictionary {:type-name label})
-          anns (annotator "before annotated test after annotated")]
+          highlighter-fn (phrases/highlighter dictionary {:type-name label})
+          anns (highlighter-fn "before annotated test after annotated")]
       (is (seq anns)))))
 
 (deftest ascii-folding-dictionary
   (let [dictionary [{:text "wörd"}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated wörd after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated wörd after annotated")]
     (is (seq anns)))
   (let [dictionary [{:text "wörd"}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated word after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated word after annotated")]
     (is (empty? anns)))
   (let [label "LABEL"
         dictionary [{:text "wörd" :ascii-fold? true}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated word after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated word after annotated")]
     (is (seq anns)))
   (let [dictionary [{:text "word" :ascii-fold? true}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated wörd after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated wörd after annotated")]
     (is (seq anns)))
   (let [label "LABEL"
         dictionary [{:text "word" :ascii-fold? false}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated wörd after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated wörd after annotated")]
     (is (empty? anns))))
 
 (deftest ascii-folding-with-case-sensitivity
   (let [label "TYPE"]
     (testing "case sensitive"
       (let [dictionary [{:text "schön" :ascii-fold? true}]
-            annotator (phrases/highlighter dictionary {:type-name label})
-            anns (annotator "before annotated Schön after annotated")]
+            highlighter-fn (phrases/highlighter dictionary {:type-name label})
+            anns (highlighter-fn "before annotated Schön after annotated")]
         (is (empty? anns)))
       (let [dictionary [{:text "Schön" :ascii-fold? true}]
-            annotator (phrases/highlighter dictionary {:type-name label})
-            anns (annotator "before annotated Schon after annotated")]
+            highlighter-fn (phrases/highlighter dictionary {:type-name label})
+            anns (highlighter-fn "before annotated Schon after annotated")]
         (is (seq anns)))
       (let [dictionary [{:text "schön" :ascii-fold? true}]
-            annotator (phrases/highlighter dictionary {:type-name label})
-            anns (annotator "before annotated Schon after annotated")]
+            highlighter-fn (phrases/highlighter dictionary {:type-name label})
+            anns (highlighter-fn "before annotated Schon after annotated")]
         (is (empty? anns))))
 
     (testing "case insensitive"
       (let [dictionary [{:text "schön" :ascii-fold? true :case-sensitive? false}]
-            annotator (phrases/highlighter dictionary {:type-name label})
-            anns (annotator "before annotated Schon after annotated")]
+            highlighter-fn (phrases/highlighter dictionary {:type-name label})
+            anns (highlighter-fn "before annotated Schon after annotated")]
         (is (seq anns))))
     (let [dictionary [{:text "schön" :ascii-fold? true :case-sensitive? false}]
-          annotator (phrases/highlighter dictionary {:type-name label})
-          anns (annotator "before annotated schon after annotated")]
+          highlighter-fn (phrases/highlighter dictionary {:type-name label})
+          anns (highlighter-fn "before annotated schon after annotated")]
       (is (seq anns)))
     (let [dictionary [{:text "schon" :ascii-fold? true :case-sensitive? false}]
-          annotator (phrases/highlighter dictionary {:type-name label})
-          anns (annotator "before annotated schön after annotated")]
+          highlighter-fn (phrases/highlighter dictionary {:type-name label})
+          anns (highlighter-fn "before annotated schön after annotated")]
       (is (seq anns)))
 
     (testing "false ascii fold"
       (let [dictionary [{:text "schon" :ascii-fold? false}]
-            annotator (phrases/highlighter dictionary {:type-name label})
-            anns (annotator "before annotated schön after annotated")]
+            highlighter-fn (phrases/highlighter dictionary {:type-name label})
+            anns (highlighter-fn "before annotated schön after annotated")]
         (is (empty? anns))))))
 
 (deftest synonyms
   (let [dictionary [{:text "test" :id "1" :synonyms ["beagle"]}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated beagle after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated beagle after annotated")]
     (is (seq anns))
     (is (= "1" (-> anns first :dict-entry-id)))
     (is (= "beagle" (-> anns first :text))))
 
   (let [dictionary [{:text "test" :id "1" :synonyms ["Luwak"] :case-sensitive? true}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated beagle after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated beagle after annotated")]
     (is (empty? anns)))
 
   (let [dictionary [{:text "test" :id "1" :synonyms ["beagle"] :case-sensitive? false}]
-        annotator (phrases/highlighter dictionary {:type-name label})
-        anns (annotator "before annotated beagle after annotated")]
+        highlighter-fn (phrases/highlighter dictionary {:type-name label})
+        anns (highlighter-fn "before annotated beagle after annotated")]
     (is (seq anns))
     (is (= "1" (-> anns first :dict-entry-id)))
     (is (= "beagle" (-> anns first :text))))
 
   (testing "synonyms with false ascii fold"
     (let [dictionary [{:text "test" :synonyms ["schön"] :ascii-fold? false}]
-          annotator (phrases/highlighter dictionary {:type-name label})
-          anns (annotator "before annotated schon after annotated")]
+          highlighter-fn (phrases/highlighter dictionary {:type-name label})
+          anns (highlighter-fn "before annotated schon after annotated")]
       (is (empty? anns)))
     (let [dictionary [{:text "test" :synonyms ["schön"] :ascii-fold? true}]
-          annotator (phrases/highlighter dictionary {:type-name label})
-          anns (annotator "before annotated schon after annotated")]
+          highlighter-fn (phrases/highlighter dictionary {:type-name label})
+          anns (highlighter-fn "before annotated schon after annotated")]
       (is (seq anns))
       (is (= "schon" (-> anns first :text))))))
 
 (deftest phrase-end-sentence
   (let [dictionary [{:text "test-test"}]
-        annotator (phrases/highlighter dictionary)
-        anns (annotator "before annotated test-test.")]
+        highlighter-fn (phrases/highlighter dictionary)
+        anns (highlighter-fn "before annotated test-test.")]
     (is (seq anns))
     (is (= "test-test" (:text (first anns))))))
 
 (deftest phrase-in-quotes
   (let [dictionary [{:text "test-test" :case-sensitive? false}]
-        annotator (phrases/highlighter dictionary)
-        anns (annotator "before annotated \"TEST-test\".")]
+        highlighter-fn (phrases/highlighter dictionary)
+        anns (highlighter-fn "before annotated \"TEST-test\".")]
     (is (seq anns))
     (is (= "TEST-test" (:text (first anns))))))
 
 (deftest phrase-in-quotes-should-not-match
   (let [dictionary [{:text "test-test" :case-sensitive? false}]
-        annotator (phrases/highlighter dictionary {:tokenizer :whitespace})
-        anns (annotator "before annotated \"TEST-test\".")]
+        highlighter-fn (phrases/highlighter dictionary {:tokenizer :whitespace})
+        anns (highlighter-fn "before annotated \"TEST-test\".")]
     (is (empty? anns))))
 
 (deftest overlapping-phrases
   (let [dictionary [{:text "test phrase test" :case-sensitive? false}]
-        annotator (phrases/highlighter dictionary {:tokenizer :whitespace})
-        anns (annotator "start test phrase test phrase test end")]
+        highlighter-fn (phrases/highlighter dictionary {:tokenizer :whitespace})
+        anns (highlighter-fn "start test phrase test phrase test end")]
     (is (= 2 (count anns)))))
 
 (deftest lt-stemming
