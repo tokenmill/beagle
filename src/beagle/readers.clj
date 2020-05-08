04ae79a987acda40e69244735ad2c53432b3c8f8
Žygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2020-05-08 09:09:21 +0300

    fix: trim CSV cells

--- src/beagle/readers.clj
+++ src/beagle/readers.clj
@@ -20,6 +20,7 @@
     (let [[header & lines] (csv/read-csv reader :separator \, :quote \")
           kvs (map keyword header)]
       (->> lines
+           (map (fn [line] (map s/trim line)))
            (map #(apply array-map (interleave kvs %)))
            (map #(into {} (remove (fn [[_ v]] (s/blank? v)) %)))
            (map (fn [{:keys [synonyms] :as dict}]
